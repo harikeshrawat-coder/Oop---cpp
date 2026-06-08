0585beef41a27a7541a031365252c1ea2c6b45a3
harikeshrawat-coder <harikeshguddu@gmail.com>
2026-06-08 23:58:16 +0530

    First OOP Program

---
+++ first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class MyClass{
+    public:
+    int myNum;
+    string myString;
+};
+
+int main(){
+    MyClass myObj;
+
+    myObj.myNum = 15;
+    myObj. myString = "Hello Harkesh";
+    cout<<myObj.myNum<<endl;
+    cout<<myObj.myString<<endl;
+
+    return 0;
+}
