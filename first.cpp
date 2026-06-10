b045d63c46bf2195b3502a5190d5eb28f9aa77fd
harikeshrawat-coder <harikeshguddu@gmail.com>
2026-06-11 00:08:14 +0530

    Added Constructor Program

--- first.cpp
+++ first.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;//using standard namespace
-
-int main(){
-    int n;
-    cin >> n; //taking input from user
-    int factorial = 1;
-    for(int i = 1; i <= n; i++){
-        factorial  = factorial * i;
+class FuncOver{
+    public:
+    void Multiplication(int x, int y){//function to multiply tow integer point values
+        cout<<"Integer Multiplication Result="<<x*y<<endl;
     }
-    cout<<"Factorial of "<<n<<" is "<<factorial<<endl;
+    void Multiplication(double x, double y){//function to multiply two double point values
+        cout<<"Double Float Result="<<x*y<<endl;
+    }
+};
 
+int main(){//main function
+    FuncOver obj;
+    obj.Multiplication(12,5);//calls show(int)
+    obj.Multiplication(3.5,5.5);//calls show(double)
     return 0;
 }
 
 
 
+    
 
 
 
