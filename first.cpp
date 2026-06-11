186006782ad83e58f493ff880a1e8cf2292816e0
harikeshrawat-coder <harikeshguddu@gmail.com>
2026-06-11 16:54:23 +0530

    Added Constructor Program

--- first.cpp
+++ first.cpp
@@ -2,13 +2,17 @@
 #include <string>
 using namespace std;//using standard namespace
 
-template <typename T>
-T maximum(T a,T b){
-    return(a>b)? a:b;
+//Example of Function Templete with multiple parameter
+template <class T>
+void fun(T a){
+    cout<<"Value of a is:"<<a<<endl;
+}
+template<class x,class y >void fun(x b,y c){
+    cout<<"Value of b is:"<<b<<endl;
+    cout<<"Value of c is:"<<c<<endl; 
 };
-
 int main(){
-    cout<<maximum(10,20)<<endl;
-    cout<<maximum(5.5,3.2)<<endl;
-    cout<<maximum('A','B')<<endl;
+    fun(10);
+    fun(20,30.5);
+    return 0;
 }
