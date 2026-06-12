5c420e6bcc92d24744c367c93c78b8af7917a0b0
harikeshrawat-coder <harikeshguddu@gmail.com>
2026-06-13 00:42:55 +0530

    Added Constructor Program

--- first.cpp
+++ first.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 using namespace std;//using standard namespace
 
-//Example of creational patterns (singleton)
+//Example of creational pattern (Factory)
 
-class Singleton{
-    private:
-    static Singleton * instance;
-
-    Singleton(){}
-
+class Shape{
     public:
-    static Singleton*getInstance(){
-        if(instance == nullptr)
-        instance = new Singleton();
-        return instance;
-    }
-
-    void show(){
-        cout<<"Singleton Instance\n";
+    virtual void draw()=0;
+};
+class Circle:public Shape{
+    public:
+    void draw(){
+        cout<<"Drawing Circle\n";
     }
 };
-
-Singleton*Singleton:: instance= nullptr;
-
+class Square:public Shape{
+    public:
+    void draw(){
+        cout<<"Drawing Square\n";
+    }
+};
+class ShapeFactory{
+    public:
+    Shape* getShape(string type){
+        if(type=="Circle")
+           return new Circle();
+        else if(type=="Square")
+           return new Square();
+        return nullptr;
+    }
+};
 int main(){
-    Singleton*s1 = Singleton::getInstance();
-    s1->show();
-    return 0; 
+    ShapeFactory factory;
+    Shape*s=factory.getShape("Circle");
+    s->draw();
+    return 0;
 }
+
